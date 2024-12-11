2f5cbf1e8a6020264cde938c369292f3e3090dc1
CoZmoTheGod <37917612+CoZmoTheGod@users.noreply.github.com>
2024-12-11 15:52:30 -0600

    fix: typo "shotting" to "shooting"

--- skills.sql
+++ skills.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS player_skills (
     citizenid VARCHAR(64) PRIMARY KEY,       -- 玩家唯一标识符
     stamina FLOAT DEFAULT 0,                  -- 体力，默认值为 0，允许小数
     strength FLOAT DEFAULT 0,                 -- 力量，默认值为 0，允许小数
-    shotting FLOAT DEFAULT 0,                 -- 射击，默认值为 0，允许小数
+    shooting FLOAT DEFAULT 0,                 -- 射击，默认值为 0，允许小数
     lung_capacity FLOAT DEFAULT 0            -- 肺活量，默认值为 0，允许小数
 );
